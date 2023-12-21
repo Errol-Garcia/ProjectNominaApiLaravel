9d7ad59c54a99defecc0c976e2bcad2e95181d55
luiskrlos27 <luisescobar74628191@gmail.com>
2023-12-21 18:45:53 -0500

    correccion form

--- ProgramPayrollFrontend/resources/views/configuration/discount/ConfigurationDiscountForm.blade.php
+++ ProgramPayrollFrontend/resources/views/configuration/discount/ConfigurationDiscountForm.blade.php
@@ -3,36 +3,49 @@
 <div class="container-sm 100% wide until small breakpoint">
     <label for="arl" class="form-label">ARL (%):</label>
     <input type="number" step="0.001" class="form-control" id="arl" name="arl"
+    @isset($accrued)
         value="{{ old('arl', $discount['arl']) }}" required>
+    @endisset
     @error('arl')
         <div class="text-small text-danger">{{ $message }}</div>
     @enderror
 
     <label for="salud" class="form-label">Salud (%):</label>
     <input type="number" step="0.001" class="form-control" id="salud" name="health"
+    @isset($accrued)
         value="{{ old('health', $discount['health']) }}" aria-describedby="emailHelp" required>
+    @endisset
     @error('health')
         <div class="text-small text-danger">{{ $message }}</div>
     @enderror
 
     <label for="s" class="form-label">Pension (%):</label>
     <input type="number" step="0.001" class="form-control" id="pension" name="pension"
+    @isset($accrued)
         value="{{ old('pension', $discount['pension']) }}" aria-describedby="emailHelp" required>
+    @endisset
     @error('pension')
         <div class="text-small text-danger">{{ $message }}</div>
     @enderror
 
     <label for="parafiscal" class="form-label">Parafiscal (%):</label>
     <input type="number" step="0.001" class="form-control" id="parafiscal" name="parafiscal"
+    @isset($accrued)
         value="{{ old('parafiscal', $discount['parafiscal']) }}" aria-describedby="emailHelp" require>
-    <input type="hidden" name="id" value="{{ old('id', $discount['id']) }}">
+    @endisset
+    <input type="hidden" name="id"
+    @isset($accrued)
+        value="{{ old('id', $discount['id']) }}">
+    @endisset
     @error('parafiscal')
         <div class="text-small text-danger">{{ $message }}</div>
     @enderror
 
     <label for="parafiscal" class="form-label">Fecha:</label>
     <input type="date" class="form-control" id="Fecha" name="registration_date"
+    @isset($accrued)
         value="{{ old('registration_date', $discount['registration_date']) }}" aria-describedby="emailHelp" require>
+    @endisset
     @error('registration_date')
         <div class="text-small text-danger">{{ $message }}</div>
     @enderror
--- ProgramPayrollFrontend/resources/views/configuration/post/PostForm.blade.php
+++ ProgramPayrollFrontend/resources/views/configuration/post/PostForm.blade.php
@@ -1,7 +1,10 @@
 @csrf
 <div class="card mx-auto">
     <label for="cargo" class="form-label">Cargos</label>
-    <input type="text" class="form-control" id="cargo" name="name"value="{{ old('name', $post['name']) }}"
+    <input type="text" class="form-control" id="cargo" name="name"
+    @isset($accrued)
+        value="{{ old('name', $post['name']) }}"
+    @endisset
         aria-describedby="emailHelp" required>
     @error('name')
         <div class="text-small text-danger">{{ $message }}</div>
